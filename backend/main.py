0433c01094fbcd9fd8df477838e3eaff04bed407
reallin <1126502397@qq.com>
2026-07-19 00:34:14 +0800

    fix

--- backend/main.py
+++ backend/main.py
@@ -804,4 +804,6 @@ def start_guava_editorial():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
